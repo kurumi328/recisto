222bb2e9203e65f4f441f5f77fd9ea5a2dad0358
shtk0llq <shtk0llq@gmail.com>
2025-01-21 10:57:16 +0900

    create page fix

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -3,39 +3,6 @@
 import styles from "./styles.module.css";
 
 export default function Create() {
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   if (
-  //     newTitle.trim() === "" ||
-  //     newCategory.trim() === "" ||
-  //     newIngredients.trim() === "" ||
-  //     newRecipe.trim() === ""
-  //   ) {
-  //     setError("タイトルとカテゴリと材料と手順を入力してください");
-  //     return;
-  //   }
-  //   try {
-  //     const addedRecord = await addTodo(
-  //       newTitle,
-  //       newCategory,
-  //       newIngredients,
-  //       newRecipe,
-  //     );
-  //     if (addedRecord) {
-  //       setRecords([addedRecord, ...records]);
-  //       setNewTitle("");
-  //       setNewCategory("");
-  //       setNewIngredients("");
-  //       setNewRecipe("");
-  //       setError("");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error adding record:", error);
-  //     setError("記録の追加に失敗しました。");
-  //   }
-  // };
-
   return (
     <div className={styles.flex}>
       <div className={styles.imgBox}>
@@ -43,12 +10,22 @@ export default function Create() {
       </div>
       <div className={styles.recipe}>
         <h1 className={styles.heading}>レシピを入力</h1>
+
         <div className={styles.uiForm}>
-          <h2 className={styles.recipeTitle}>レシピタイトル</h2>
-          <h3 className={styles.contentTitle}>材料</h3>
-          <dl className={styles.ingredientList}>材料を入力...</dl>
-          <h2 className={styles.contentTitle}>作り方</h2>
-          <ol className={styles.stepList}>手順を入力...</ol>
+          <div>
+            <label className={styles.label} htmlFor="title">レシピタイトル</label>
+            <input type="text" id="title" className={styles.recipeTitleInput} />
+          </div>
+
+          <div>
+            <label className={styles.label} htmlFor="ingredients">材料</label>
+            <textarea placeholder="材料を入力..." className={styles.ingredientInput}></textarea>
+          </div>
+
+          <div>
+            <label className={styles.label} htmlFor="content">作り方</label>
+            <textarea type="text" placeholder="手順を入力..." className={styles.contentInput}></textarea>
+          </div>
         </div>
 
         <button className={styles.createButton}>登録</button>
