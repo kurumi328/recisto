3f449909d24dc368166cf7b40de4a2a02d4b18ff
shtk0llq <shtk0llq@gmail.com>
2025-01-21 10:28:33 +0900

    recipe create page reset

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -1,64 +1,40 @@
-import React, { useState } from "react";
+"use client";
+
 import styles from "./styles.module.css";
-import { supabase } from "../utils/supabase";
-import { getAllRecords, addTodo, deleteTodo } from "../utils/supabaseFunctions";
 
 export default function Create() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [records, setRecords] = useState([]);
-  const [newTitle, setNewTitle] = useState("");
-  const [newCategory, setNewCategory] = useState("");
-  const [newIngredients, setNewIngredients] = useState("");
-  const [newRecipe, setNewRecipe] = useState("");
-  const [error, setError] = useState("");
 
-  useEffect(() => {
-    getRecords();
-  }, []);
-
-  const getRecords = async () => {
-    try {
-      const fetchedRecords = await getAllRecords();
-      setRecords(fetchedRecords);
-      setIsLoading(false);
-    } catch (error) {
-      console.error("Error fetching records:", error);
-      setError("データの取得に失敗しました。");
-      setIsLoading(false);
-    }
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (
-      newTitle.trim() === "" ||
-      newCategory.trim() === "" ||
-      newIngredients.trim() === "" ||
-      newRecipe.trim() === ""
-    ) {
-      setError("タイトルとカテゴリと材料と手順を入力してください");
-      return;
-    }
-    try {
-      const addedRecord = await addTodo(
-        newTitle,
-        newCategory,
-        newIngredients,
-        newRecipe,
-      );
-      if (addedRecord) {
-        setRecords([addedRecord, ...records]);
-        setNewTitle("");
-        setNewCategory("");
-        setNewIngredients("");
-        setNewRecipe("");
-        setError("");
-      }
-    } catch (error) {
-      console.error("Error adding record:", error);
-      setError("記録の追加に失敗しました。");
-    }
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   if (
+  //     newTitle.trim() === "" ||
+  //     newCategory.trim() === "" ||
+  //     newIngredients.trim() === "" ||
+  //     newRecipe.trim() === ""
+  //   ) {
+  //     setError("タイトルとカテゴリと材料と手順を入力してください");
+  //     return;
+  //   }
+  //   try {
+  //     const addedRecord = await addTodo(
+  //       newTitle,
+  //       newCategory,
+  //       newIngredients,
+  //       newRecipe,
+  //     );
+  //     if (addedRecord) {
+  //       setRecords([addedRecord, ...records]);
+  //       setNewTitle("");
+  //       setNewCategory("");
+  //       setNewIngredients("");
+  //       setNewRecipe("");
+  //       setError("");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error adding record:", error);
+  //     setError("記録の追加に失敗しました。");
+  //   }
+  // };
 
   return (
     <div className={styles.flex}>
@@ -67,34 +43,15 @@ export default function Create() {
       </div>
       <div className={styles.recipe}>
         <h1 className={styles.heading}>レシピを入力</h1>
-        {error && <p className="error">{error}</p>}
-        <div className={styles.uiForm} onSubmit={handleSubmit}>
-          <h2
-            className={styles.recipeTitle}
-            title={newTitle}
-            setTitle={setNewTitle}
-          />
-          <input
-            type="text"
-            value={newTitle}
-            onChange={(e) => setNewTitle(e.target.value)}
-          />
-          +<h3 className={styles.contentTitle}>材料</h3>
-          <dl
-            className={styles.ingredientList}
-            title={newIngredients}
-            setTitle={setNewIngredients}
-          />
+        <div className={styles.uiForm}>
+          <h2 className={styles.recipeTitle}>レシピタイトル</h2>
+          <h3 className={styles.contentTitle}>材料</h3>
+          <dl className={styles.ingredientList}>材料を入力...</dl>
           <h2 className={styles.contentTitle}>作り方</h2>
-          <ol
-            className={styles.stepList}
-            title={newRecipe}
-            setTitle={setNewRecipe}
-          />
+          <ol className={styles.stepList}>手順を入力...</ol>
         </div>
-        <button className={styles.createButton} onClick={handleCommentSubmit}>
-          登録 　　　　　　　
-        </button>
+
+        <button className={styles.createButton}>登録</button>
       </div>
     </div>
   );
