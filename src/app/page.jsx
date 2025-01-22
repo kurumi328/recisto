e50d9e2195b085d4d53f06a563fd58eb59be7821
kurumi <milkcoffe43@gmail.com>
2025-01-22 23:00:50 +0900

    home-page fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,18 +20,17 @@ export default function Home() {
       <h1 className={styles.heading}>ALL</h1>
       <div className={styles.cardArea}>
         {recipes.map((recipe) => (
-          <a key={recipe.recipe_id} className={styles.cardItem} href="#">
-            <div className={styles.imgBox}>
+            <a key =[], className={styles.imgBox}>
               <img
                 className={styles.cardItemImg}
                 src="./images/pc01.png"
                 alt=""
               />
-            </div>
+            </a>
             <div className={styles.cardItems}>
               <p className={styles.cardItemTtl}>{recipe.title}</p>
             </div>
-          </a>
+          </Liink>
         ))}
       </div>
     </div>
