43f17f08def0419307c2e853c67b14e106d7b417
shtk0llq <shtk0llq@gmail.com>
2025-01-26 16:50:49 +0900

    signin feature create

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,29 +1,63 @@
 "use client";
 
+import Link from "next/link";
 import styles from "./styles.module.css";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { auth } from "../../libs/Firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+
+      router.push("/");
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   return (
     <div className={styles.formContainer}>
       <div className={styles.uiForm}>
-        <input
-          className={styles.userName}
-          type="text"
-          placeholder="ユーザー名"
-        ></input>
-        <input
-          className={styles.mailAddress}
-          type="text"
-          placeholder="メールアドレス"
-        ></input>
-        <input
-          className={styles.password}
-          type="text"
-          placeholder="パスワード"
-        ></input>
+        <h2>ログイン</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            className={styles.mailAddress}
+            type="text"
+            placeholder="メールアドレス"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-        <button className={styles.signin}>ログイン</button>
-        <div className={styles.signup}>アカウントをお持ちでない場合は登録</div>
+          <input
+            className={styles.password}
+            type="text"
+            placeholder="パスワード"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit" className={styles.signin}>
+            ログイン
+          </button>
+        </form>
+
+        <Link href={"/signUp"} className={styles.signup}>
+          アカウントをお持ちでない場合は登録
+        </Link>
       </div>
     </div>
   );
