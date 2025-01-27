50af683e46c9030b4816612b1bac2beb602d3a8d
shtk0llq <shtk0llq@gmail.com>
2025-01-27 20:22:13 +0900

    api key secret

--- src/libs/Firebase.jsx
+++ src/libs/Firebase.jsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCn0Ig1cjEV_DkOcEVdDjArpxA3lc1W2k0",
-  authDomain: "recisto-89e8a.firebaseapp.com",
-  projectId: "recisto-89e8a",
-  storageBucket: "recisto-89e8a.firebasestorage.app",
-  messagingSenderId: "911296742813",
-  appId: "1:911296742813:web:2ca13cac444e6b6e799b74",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
